71d12a8e4d721e905255e35d9d1dadf3e4c49283
Sambath Ranchaya <sambath.ranchaya@e-legalinc.com>
2022-07-06 16:48:22 -0500

    completed

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import re
-import long_responses
+import long_responses as long
+
 
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
-    has_required_words= True
+    has_required_words = True
 
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
-#   calculate the percent of recognised words in a user message
+    #   calculate the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
     for word in required_words:
@@ -18,25 +19,33 @@ def message_probability(user_message, recognised_words, single_response=False, r
             break
 
     if has_required_words or single_response:
-        return int(percentage*100)
+        return int(percentage * 100)
     else:
         return 0
+
 
 def check_all_messages(message):
     highest_prob_list = {}
 
-    def response(bot_response, list_of_words, singe_response = False , required_words = []):
+    def response(bot_response, list_of_words, singe_response=False, required_words=[]):
         nonlocal highest_prob_list
         highest_prob_list[bot_response] = message_probability(message, list_of_words, singe_response, required_words)
 
         # Response ---------------------------------------------------
-        response ('Hello!' , ['hello', 'hi' , 'sup', 'hey', 'heyo'], single_response = True)
-        response ('I\'m doing fine, and you?', ['how' , 'are' , 'you' , 'doing'], required_words=['how'])
+        response('Hello!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
+        response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+        response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+
+        best_match = max(highest_prob_list, key=highest_prob_list.get)
+        print(highest_prob_list)
+
+        return best_match
 
 
 def get_response(user_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
     response = check_all_messages(split_message)
+
     return response
 
 
